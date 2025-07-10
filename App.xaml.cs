c87b688bd5be25578d036fed8c0e5c302b3e6e05
Azok42 <anton1.hackner@gmail.com>
2025-07-10 09:59:52 +0200

    generate general WPF structure

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sand_Simulation_with_WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
