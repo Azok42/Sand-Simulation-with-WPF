5d7b646185e6305ce95a9531d70353ca177e6c24
Azok42 <anton1.hackner@gmail.com>
2025-07-11 16:05:11 +0200

    added iteraction logic for grid+ small clean ups

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿using System.Reflection;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,36 +13,50 @@ using System.Windows.Shapes;
 
 namespace Sand_Simulation_with_WPF;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    Grid baseGrid = new Grid();
+
     public MainWindow()
     {
         InitializeComponent();
-        double GRID_SIZE = 15.0;
+
+        genGrid();
+    }
+
+    private void genCell(object sender, RoutedEventArgs e) {
+        if(System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed)
+        {
+            Rectangle rect = sender as Rectangle;
+            rect.Fill = System.Windows.Media.Brushes.Blue;
+        };
+    }
+
+    private void genGrid() {
+        double GRID_SIZE = 40.0;
         double screenWidth = System.Windows.SystemParameters.WorkArea.Width;
         double screenHeight = System.Windows.SystemParameters.WorkArea.Height;
+        int cellHeightCount = (int)(screenHeight / GRID_SIZE);
+        int cellWidthCount = (int)(screenWidth / GRID_SIZE);
 
-
-        Grid baseGrid = new Grid();
         baseGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
         baseGrid.VerticalAlignment = VerticalAlignment.Stretch;
         baseGrid.ShowGridLines = true;
 
-        RowDefinition[] rowDefinition = new RowDefinition[(int)(screenHeight / GRID_SIZE)];
-        for (int i = 0; i < (int)(screenHeight / GRID_SIZE); i++) {
-            rowDefinition[i] = new RowDefinition();
-            rowDefinition[i].Height = new GridLength(GRID_SIZE);
-            baseGrid.RowDefinitions.Add(rowDefinition[i]);
+        for (int i = 0; i < cellHeightCount; i++) {
+            baseGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(GRID_SIZE) });
         }
 
-        ColumnDefinition[] columnDefinition = new ColumnDefinition[(int)(screenWidth / GRID_SIZE)];
-        for (int i = 0; i < (int)(screenWidth / GRID_SIZE); i++) {
-            columnDefinition[i] = new ColumnDefinition();
-            columnDefinition[i].Width = new GridLength(GRID_SIZE);
-            baseGrid.ColumnDefinitions.Add(columnDefinition[i]);
+        for (int i = 0; i < cellWidthCount; i++) {
+            baseGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(GRID_SIZE) });
+            for (int j = 0; j < cellHeightCount; j++) {
+                Rectangle rect = new Rectangle();
+                rect.MouseEnter += genCell;
+                Grid.SetRow(rect, j);
+                Grid.SetColumn(rect, i);
+                rect.Fill = System.Windows.Media.Brushes.Transparent;
+                baseGrid.Children.Add(rect);
+            }
         }
 
         this.Content = baseGrid;
