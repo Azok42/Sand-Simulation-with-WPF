53d14ebd9b18132201f167b6212b282581dfc920
Azok42 <anton1.hackner@gmail.com>
2025-07-31 13:22:02 +0200

    optimisations

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,19 +16,19 @@ namespace Sand_Simulation_with_WPF;
 
 public partial class MainWindow : Window
 {
-    static double GRID_SIZE = 40.0;
+    static double GRID_SIZE = 20.0;
     static double screenWidth = System.Windows.SystemParameters.WorkArea.Width;
     static double screenHeight = System.Windows.SystemParameters.WorkArea.Height;
     public static int cellHeightCount = (int)(screenHeight / GRID_SIZE) - 1;
     public static int cellWidthCount = (int)(screenWidth / GRID_SIZE) - 1 ;       
-    static Grid baseGrid = new Grid();
-    static List<Cell> cells = new List<Cell>();
-
+    public static Grid baseGrid = new Grid();
+    public static Cell[,] currentCells = new Cell[cellHeightCount, cellWidthCount];
+    public static Cell[,] nextCells = new Cell[cellHeightCount, cellWidthCount];
 
     public MainWindow()
     {
         InitializeComponent();
-
+	
         genGrid();
 	mainLoop();
     }
@@ -37,33 +37,46 @@ public partial class MainWindow : Window
     {
 	while(true)
 	{
-	    foreach(Cell cell in cells)
+	    foreach(Cell cell in currentCells)
 	    {
-		cell.update();	
-	    }
+		if(cell == null)
+		    continue;
 
-	    await Task.Delay(30);
+		cell.update();
+	    }
+	   
+	    for (int i = 0; i < cellHeightCount; i++)
+	    {
+    		for (int j = 0; j < cellWidthCount; j++)
+    		{
+        	    currentCells[i, j] = nextCells[i, j];
+    		}
+	    }
+	    
+	    await Task.Delay(33);
 	}	
     }
 
     private void genCell(object sender, RoutedEventArgs e)
     {
         Rectangle rect = (Rectangle)sender;
-
-        if(System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed && !isCellAtGridPos(Grid.GetRow(rect), Grid.GetColumn(rect)))
+	int row = Grid.GetRow(rect);
+	int col = Grid.GetColumn(rect);
+	
+        if(System.Windows.Input.Mouse.LeftButton == MouseButtonState.Pressed && nextCells[row, col] == null)
         {
 	    
-	    Sand sand = new Sand(Grid.GetRow(rect), Grid.GetColumn(rect));
-	    baseGrid.Children.Add(sand.rect);
-	    cells.Add(sand);
-        };
+	    Sand sand = new Sand(row, col);
+	    nextCells[row, col] = sand;
+	    baseGrid.Children.Add(nextCells[row, col].rect);
+        }
     }
 
     public void genGrid()
     {
        	baseGrid.HorizontalAlignment = HorizontalAlignment.Center;
         baseGrid.VerticalAlignment = VerticalAlignment.Center;
-        baseGrid.ShowGridLines = true;
+        baseGrid.ShowGridLines = false;
 
         for (int i = 0; i < cellHeightCount; i++) {
             baseGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(GRID_SIZE) });
@@ -74,7 +87,7 @@ public partial class MainWindow : Window
             for (int j = 0; j < cellHeightCount; j++) {
                 Rectangle rect = new Rectangle();
                 rect.MouseEnter += genCell;
-                rect.MouseLeftButtonDown += genCell;
+		rect.MouseLeftButtonDown += genCell;
                 Grid.SetRow(rect, j);
                 Grid.SetColumn(rect, i);
                 rect.Fill = System.Windows.Media.Brushes.Silver;
@@ -83,32 +96,22 @@ public partial class MainWindow : Window
         }
 
         this.Content = baseGrid;
-    }
-    
-    static public bool isCellAtGridPos(int row, int col)
-    {
-	foreach(Cell cell in cells)
-	{
-	    if(Grid.GetRow(cell.rect) == row && Grid.GetColumn(cell.rect) == col)
-		return true;
-	}
-	
-	return false;
-    }
+    } 
 }
 
 public abstract class Cell
 {
     public Rectangle rect = new Rectangle();
     public abstract void update();
+    public int row, col;
 }
 
 public class Sand : Cell
 {
-    int row, col;
-
     public Sand(int row, int col)
     {
+	this.row = row;
+	this.col = col;
         Grid.SetRow(rect, row);
         Grid.SetColumn(rect, col);
         rect.Fill = System.Windows.Media.Brushes.Yellow;
@@ -116,29 +119,43 @@ public class Sand : Cell
 
     public override void update()
     {
-	row = Grid.GetRow(rect);
-	col = Grid.GetColumn(rect);
+	if(row+1 >= MainWindow.cellHeightCount)
+	    return;
 
 	//Move down if nothing is under it
-        if(!MainWindow.isCellAtGridPos(row+1, col) && MainWindow.cellHeightCount >= row+1)
+        if(MainWindow.nextCells[row+1, col] == null)
 	{
 	    Grid.SetRow(rect, row+1);
+	    MainWindow.currentCells[row, col] = null;
+	    MainWindow.nextCells[row+1, col] = this;
+	    MainWindow.nextCells[row, col] = null;
+	    row++;
 	    return;
 	}
 	
 	//Move right-down if nothing is there
-	if(!MainWindow.isCellAtGridPos(row+1, col+1) && MainWindow.cellWidthCount >= col+1 && MainWindow.cellHeightCount >= row+1)
+	if(col+1 < MainWindow.cellWidthCount && MainWindow.nextCells[row+1, col+1] == null)
 	{
 	    Grid.SetRow(rect, row+1);
 	    Grid.SetColumn(rect, col+1);
+	    MainWindow.currentCells[row, col] = null;
+	    MainWindow.nextCells[row+1, col+1] = this;
+	    MainWindow.nextCells[row, col] = null;
+	    row++;
+	    col++;
 	    return;
 	}
 
 	//Move left-down if nothing is there
-	if(!MainWindow.isCellAtGridPos(row+1, col-1) && 0  >= col-1 && MainWindow.cellHeightCount >= row+1)
+	if(col-1 >= 0 && MainWindow.nextCells[row+1, col-1] == null) 
 	{
 	    Grid.SetRow(rect, row+1);
 	    Grid.SetColumn(rect, col-1);
+	    MainWindow.currentCells[row, col] = null;
+	    MainWindow.nextCells[row+1, col-1] = this;
+	    MainWindow.nextCells[row, col] = null;
+	    row++;
+	    col--;
 	    return;
 	}
     }
