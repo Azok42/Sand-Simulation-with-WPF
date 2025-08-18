be075c0fb1d37410ce81a30c5159c0d8d4000a86
Azok42 <anton1.hackner@gmail.com>
2025-08-18 19:12:33 +0200

    created configWindow (WIP)

---
+++ ConfigWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sand_Simulation_with_WPF;
+
+public partial class ConfigWindow : Window
+{
+    	public ConfigWindow()
+    	{
+        	InitializeComponent();
+	}
+}
